81ea5ba306f13c2fbee074d937fb61b43872069f
Macbook <macbook@MacBook-Air-Macbook.local>
2026-07-17 15:58:59 +0300

    Add Google Ads tag

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { getStaticLanguageParams } from "@/lib/i18n";
 import "../globals.css";
 
@@ -29,6 +30,21 @@ export default async function LangRootLayout({
 
   return (
     <html lang={lang}>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18329572887"
+        />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {\`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18329572887');
+          \`}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
