dbf51fd3654db25aff69e36732472c8dcff84224
Macbook <macbook@MacBook-Air-Macbook.local>
2026-07-22 15:29:11 +0300

    fix: remove '/-' basePath to fix styling on custom domain recruitingworkinbelarus.com

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-const basePath = isGitHubPages ? '/-' : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
